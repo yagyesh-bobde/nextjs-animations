59cb0709edcda3304a75153de19b343326586d7d
Yagyesh Bobde <yagyeshSRbobde@gmail.com>
2024-03-19 00:42:37 +0530

    custom use dimension hook

---
+++ hooks/useDimensions.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const useDimensions = () => {
+    const [dimensions, setDimensions] = useState({ width : 0, height: 0 });
+
+    const updateDimensions = () => {
+        const { innerWidth, innerHeight } = window;
+        setDimensions({ width: innerWidth, height: innerHeight });
+    }
+
+    useEffect(() => {
+        updateDimensions();
+
+        window.addEventListener("resize", updateDimensions);
+
+        return () => {
+            window.removeEventListener("resize", updateDimensions);    
+        }
+    }, [])
+}
+
+
+export default useDimensions;
